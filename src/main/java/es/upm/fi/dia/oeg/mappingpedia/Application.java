fdafe1ab9ef9b95cbbafd57a6dae5e2d78ec2f6d
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-06-26 17:31:44 +0200

    bug fixes

--- src/main/java/es/upm/fi/dia/oeg/mappingpedia/Application.java
+++ src/main/java/es/upm/fi/dia/oeg/mappingpedia/Application.java
@@ -11,19 +11,19 @@ import org.springframework.boot.autoconfigure.web.MultipartProperties;
 
 
 @SpringBootApplication
-public class ExecutionsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger("ExecutionsApplication");
+		Logger logger = LoggerFactory.getLogger("Application");
 		logger.info("Working Directory = " + System.getProperty("user.dir"));
-		logger.info("Starting Mappingpedia Engine ExecutionsApplication version 1.0.0 ...");
-		SpringApplication.run(ExecutionsApplication.class, args);
+		logger.info("Starting Mappingpedia Engine Application version 1.0.0 ...");
+		SpringApplication.run(Application.class, args);
 
 		MultipartProperties multipartProperties = new MultipartProperties();
 		multipartProperties.setLocation("./mpe-executions-ws-temp");
 		String multiPartPropertiesLocation = multipartProperties.getLocation();
 		logger.info("multiPartPropertiesLocation = " + multiPartPropertiesLocation);
 
-		logger.info("Mappingpedia Engine ExecutionsApplication WS started.\n\n\n");
+		logger.info("Mappingpedia Engine Application WS started.\n\n\n");
 	}
 }
